0d3b42995f0e5dc8b2d15ba924c5055500be14f1
noobshubham <noobshubham65@gmail.com>
2022-03-12 10:37:24 +0530

    Fixed Splash Screen Crash!

--- app/src/main/java/com/noobshubham/exmovieapp/SplashFragment.kt
+++ app/src/main/java/com/noobshubham/exmovieapp/SplashFragment.kt
@@ -3,18 +3,14 @@ package com.noobshubham.exmovieapp
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
-import java.util.*
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
-import kotlin.concurrent.schedule
-import kotlin.concurrent.timerTask
+import kotlinx.coroutines.flow.combine
 
 class SplashFragment : Fragment() {
 
@@ -29,11 +25,13 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         // delay handler or https://stackoverflow.com/a/41664501
         Handler(Looper.getMainLooper()).postDelayed({
             findNavController().navigate(R.id.action_splashFragment_to_FirstFragment)
             //Do something after 2s
         }, 2000)
+
     }
 
     // code to hide the action bar https://stackoverflow.com/a/56721357
